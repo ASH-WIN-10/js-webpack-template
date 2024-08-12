7521e719cb346727decc917bbeb24bef465a203c
Ashvin <ashvinjangid10@gmail.com>
2024-08-12 18:59:58 +0530

    add webpack-merge and configure npm scripts

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { type } = require("os");
 
 module.exports = {
 	entry: "./src/index.js",
-	mode: "development",
 	output: {
 		filename: "index.js",
 		path: path.resolve(__dirname, "dist"),
 		clean: true,
-	},
-	devtool: "eval-source-map",
-	devServer: {
-		watchFiles: ["./src/template.html"],
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
